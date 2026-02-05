5a41c99dd362f7eea257098b5a80bf0bc4d423db
RobinJames25 <jamesrobin12432@gmail.com>
2026-02-05 07:42:58 +0300

    Updated portfolio with a new project Personal Blog Website

--- client/src/pages/Portfolio.jsx
+++ client/src/pages/Portfolio.jsx
@@ -9,6 +9,7 @@ import ProductListCartComponent  from '../assets/ProductListCartComponent.png';
 import BentoGridChallenge  from '../assets/BentoGridChallenge.png';
 import RandomQuoteGenerator  from '../assets/RandomQuoteGenerator.png';
 import PersonalFinanceApp from '../assets/PersonalFinanceApp.png';
+import PersonalBlogWebsite from '../assets/PersonalBlogWebsite.png';
 import { useState } from 'react';
 
 
@@ -28,6 +29,7 @@ export default function Portfolio({ isActive }) {
         { id: 8, title: 'Bento Grid Challenge', category: 'Web development', img: BentoGridChallenge, url: 'https://robinjames25.github.io/Bento-Grid-Challenge/' },
         { id: 9, title: 'Random Quote Generator', category: 'Web development', img: RandomQuoteGenerator, url: 'https://quote-generator-nine-mu.vercel.app/' },
         { id: 10, title: 'Personal Finance Application', category: 'Web development', img: PersonalFinanceApp, url: 'https://personal-finance-app-theta-dun.vercel.app/' },
+        { id: 11, title: 'Personal Blog Website', category: 'Web development', img: PersonalBlogWebsite, url: 'https://personal-blog-seven-omega.vercel.app/' },
     ]
 
     const filteredProjects = projects.filter(project =>
